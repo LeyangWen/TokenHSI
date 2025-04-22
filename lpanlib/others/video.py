53a3d0132079c6ef27090f7410ac81f91d4f75bf
LeyangWen <wenleyan@umich.edu>
2025-04-22 19:00:13 -0400

    local_visualization

--- lpanlib/others/video.py
+++ lpanlib/others/video.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser.add_argument("--imgs_dir", type=str, required=True)
     parser.add_argument("--fps", type=int, default=30)
     parser.add_argument("--delete_imgs", action="store_true", default=False)
-
+    parser.add_argument("--video_name", type=str, default="video")
     args = parser.parse_args()
 
     imgs_dir = args.imgs_dir
@@ -17,11 +17,13 @@ if __name__ == "__main__":
     fnames = os.listdir(imgs_dir)
     fnames = sorted(fnames)
 
+    print(f"{os.path.join(imgs_dir, fnames[0])} -> {os.path.join(imgs_dir, fnames[-1])}")
+    
     imgs = [cv2.imread(os.path.join(imgs_dir, fn), cv2.IMREAD_COLOR) for fn in fnames]
-
+    
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     size = imgs[0].shape[:2]
-    videoWriter = cv2.VideoWriter(os.path.join(imgs_dir, "video.mp4"), fourcc, fps, (size[1], size[0]))
+    videoWriter = cv2.VideoWriter(os.path.join(imgs_dir, f"{args.video_name}.mp4"), fourcc, fps, (size[1], size[0]))
 
     for i in range(len(imgs)):
         # cv2.imshow("test", imgs[i])
@@ -30,7 +32,7 @@ if __name__ == "__main__":
     
     videoWriter.release()
 
-    print("video saved at {}".format(os.path.join(imgs_dir, "video.mp4")))
+    print("video saved at {}".format(os.path.join(imgs_dir, f"{args.video_name}.mp4")))
 
     if delete_imgs:
         for i in range(len(imgs)):
--- run_gui.sh
+++ run_gui.sh
@@ -1,20 +1,69 @@
 # conda activate tokenhsi # need python 3.8, so you cant load python3.10-anaconda etc, or used the module load pytorch
 
 
+# Basic Carry Test
+# python -u ./tokenhsi/run.py --task HumanoidCarry \
+#     --cfg_train tokenhsi/data/cfg/train/rlg/amp_imitation_task.yaml \
+#     --cfg_env tokenhsi/data/cfg/basic_interaction_skills/amp_humanoid_carry_construction.yaml \
+#     --motion_file tokenhsi/data/dataset_carry/dataset_carry.yaml \
+#     --checkpoint output/single_task/ckpt_carry.pth \
+#     --test \
+#     --num_envs 1 \
+#     --wandb_project "TokenHSI-Test" \
+#     --wandb_name "Carry_test_1" \
+#     --wandb_mode "disabled" \
+#     --box_w 0.60 \
+#     --box_l 0.75 \
+#     --box_h 0.50 \
+#     --random_size False \
+#     --random_density False \
+#     --random_mode_equal_proportion False \
 
+
+    # # Small box
+    # --box_w 0.25 \
+    # --box_l 0.30 \
+    # --box_h 0.20 \
+
+    # # Medium box
+    # --box_w 0.35 \
+    # --box_l 0.45 \
+    # --box_h 0.30 \
+
+    # # Large box
+    # --box_w 0.45 \
+    # --box_l 0.60 \
+    # --box_h 0.40 \
+
+    # # Extra large box
+    # --box_w 0.60 \
+    # --box_l 0.75 \
+    # --box_h 0.50 \
+
+
+
+
+# # Terrain Carry Test
 python ./tokenhsi/run.py --task HumanoidAdaptCarryGround2Terrain \
     --cfg_train tokenhsi/data/cfg/train/rlg/amp_imitation_task_transformer_multi_task_adapt.yaml \
     --cfg_env tokenhsi/data/cfg/adapt_interaction_skills/amp_humanoid_adapt_carry_ground2terrain_construction.yaml \
     --motion_file tokenhsi/data/dataset_carry/dataset_carry.yaml \
     --hrl_checkpoint output/tokenhsi/ckpt_stage1.pth \
     --checkpoint output/tokenhsi/ckpt_stage2_terrainShape_carry.pth \
-    --output_path /scratch/shdpm_root/shdpm0/wenleyan/tokenhsi/carry_terrain1/ \
     --test \
     --num_envs 1 \
     --wandb_project "TokenHSI-Test" \
     --wandb_name "CarryTerrain_test" \
     --wandb_mode "disabled" \
-    --notes "rand loc, test carry" \
+    --Notes "rand loc, test carry" \
+    --box_w 0.40 \
+    --random_size False \
+    --random_density False \
+    --random_mode_equal_proportion False \
+
 
 
 # sh tokenhsi/scripts/single_task/traj_test.sh
+
+
+# python lpanlib/others/video.py --imgs_dir "output/imgs/2025-04-22_18-56-01" --video_name "terrain.mp4" --delete_imgs 
