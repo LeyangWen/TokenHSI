b35ee792f1f517b798a22764b26c770de6ad6501
LeyangWen <wenleyan@umich.edu>
2026-02-03 02:19:47 -0500

    120s

--- slurm_train_MMH_handle.sh
+++ slurm_train_MMH_handle.sh
@@ -148,9 +148,9 @@ python -u ./tokenhsi/run.py --task HumanoidCarry \
     --construction_experiment False \
     --random_density True \
     --motion_file tokenhsi/data/dataset_carry/dataset_MMH_handle.yaml \
-    --output_path /scratch/shdpm_root/shdpm0/wenleyan/tokenhsi/MMH-Try3/Carry-handle-train-7/ \
-    --wandb_name Try3-Carry-handle-train-7 \
-    --notes "more imitation motion" \
+    --output_path /scratch/shdpm_root/shdpm0/wenleyan/tokenhsi/MMH-Try3/Carry-handle-train-8/ \
+    --wandb_name Try3-Carry-handle-train-8 \
+    --notes "more imitation motion, 120s" \
     --ergo_coeff 0.2 \
     --resume 1 \
     --checkpoint /scratch/shdpm_root/shdpm0/wenleyan/tokenhsi/MMH-Try3/Carry-handle-train-6/Humanoid_02-02-52-00/nn/Humanoid.pth \
